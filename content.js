75f36b23aef4adfba9e38cd38ad0fa6e85b7caac
Felipe Hollerbach <felipe.hollerbach@gmail.com>
2022-05-09 21:48:23 -0300

    working on live games now

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-/*Code completely insipired by elibretto's extension at: https://chrome.google.com/webstore/detail/nba-timezone-extension/lpbnlpmmjelgjabfaopaalpficcbinjj?hl=en-GB
+/*Code insipired by elibretto's extension at: https://chrome.google.com/webstore/detail/nba-timezone-extension/lpbnlpmmjelgjabfaopaalpficcbinjj?hl=en-GB
 I only updated his extension to work in 2022 out of necessity since it wasnt update since 2018*/
 
 //ScoreboardGame_gameStatusText__wwj1V | Div Home Page
@@ -6,6 +6,36 @@ I only updated his extension to work in 2022 out of necessity since it wasnt upd
 //ScheduleStatusText_base__R5PI0  |  Div Schedule
 
 //maybe regex will be needed in the future if Div class changes, example: document.querySelectorAll('div[class^="ScoreboardGame_gameStatusText"]');
+
+function change(x){
+  for(var i = 0; i < x.length; i++){
+    t = x[i].innerHTML
+
+    bits = t.split(/[\s]+/);
+
+    if(bits[0] != 'Q1' && bits[0] != 'Q2' && bits[0] != 'Q3' && bits[0] != 'Q4' && bits [0] != 'End' && bits [0] != 'Half'){ // Making so the algorithm wont run if the game is already live.
+      t = bits[0];
+      split = t.split(/[:]+/);
+
+      t = parseInt(split[0]) - offset;
+
+      if(t >= 12 && bits[1] == "PM") {
+        bits[1] = "AM";
+        if (t > 12) {
+          t = t - 12;
+        }
+      }
+          
+      if(t >= 12 && bits[1] == "AM") {
+        bits[1] = "PM";
+        if (t > 12) {
+          t = t - 12;
+        }
+      }
+      x[i].innerHTML = t + ":" + split[1] + " " + bits[1];
+    }
+  }
+}
 
 window.onload =
 function run() {
@@ -17,90 +47,14 @@ function run() {
 
   timesRun = 0;
 
-  x=document.querySelectorAll('div[class^="ScoreboardGame_gameStatusText"]');
-    for(var i = 0; i < x.length; i++){
+  x=document.querySelectorAll('div[class^="ScoreboardGame_gameStatusText"]'); // Home Page
+  change(x);
 
-      t = x[i].innerHTML
+  y=document.getElementsByClassName('h9 text-xs uppercase'); //Games Tab
+  change(y);
 
-      bits = t.split(/[\s]+/);
-      t = bits[0];
-      split = t.split(/[:]+/);
+  z=document.getElementsByClassName('ScheduleStatusText_base__R5PI0'); // Schedule Tab
+  change(z);
 
-      t = parseInt(split[0]) - offset;
-
-      if(t >= 12 && bits[1] == "PM") {
-        bits[1] = "AM";
-        if (t > 12) {
-          t = t - 12;
-         }
-      }
-            
-      if(t >= 12 && bits[1] == "AM") {
-        bits[1] = "PM";
-        if (t > 12) {
-          t = t - 12;
-        }
-      }
-
-      x[i].innerHTML = t + ":" + split[1] + " " + bits[1];
-
-    }
-
-
-    y=document.getElementsByClassName('h9 text-xs uppercase')
-    for(var i = 0; i < y.length; i++){
-
-      t = y[i].innerHTML
-
-      bits = t.split(/[\s]+/);
-      t = bits[0];
-      split = t.split(/[:]+/);
-
-      t = parseInt(split[0]) - offset;
-
-      if(t >= 12 && bits[1] == "PM") {
-        bits[1] = "AM";
-        if (t > 12) {
-          t = t - 12;
-         }
-      }
-            
-      if(t >= 12 && bits[1] == "AM") {
-        bits[1] = "PM";
-        if (t > 12) {
-          t = t - 12;
-        }
-      }
-
-      y[i].innerHTML = t + ":" + split[1] + " " + bits[1];
-
-    }
-
-    z=document.getElementsByClassName('ScheduleStatusText_base__R5PI0')
-    for(var i = 0; i < z.length; i++){
-      t = z[i].innerHTML
-
-      bits = t.split(/[\s]+/);
-      t = bits[0];
-      split = t.split(/[:]+/);
-
-      t = parseInt(split[0]) - offset;
-
-      if(t >= 12 && bits[1] == "PM") {
-        bits[1] = "AM";
-        if (t > 12) {
-          t = t - 12;
-         }
-      }
-            
-      if(t >= 12 && bits[1] == "AM") {
-        bits[1] = "PM";
-        if (t > 12) {
-          t = t - 12;
-        }
-      }
-
-      z[i].innerHTML = t + ":" + split[1] + " " + bits[1];
-
-    }
+  //window.addEventListener('click', setTimeout(run, 2000));   trying to make Games Tab update every time you see a different date.
 }
